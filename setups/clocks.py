7181a4a443ab9893842d66792bbb0ddcdc5f577b
Julian Metzler <git@mezgr.de>
2016-04-06 19:14:28 +0200

    Added weather script and updated fonts

--- setups/clocks.py
+++ setups/clocks.py
@@ -10,7 +10,7 @@ client.add_graphics_submessage('panel', 'text', text = "%H%M", timestring = True
 
 client.add_graphics_submessage('side', 'text', text = "%d.%m.%y", timestring = True, font = "Itty", size = 4, x = 0, y = 0, refresh_interval = 'minute')
 client.add_graphics_submessage('side', 'binary_clock', block_width = 4, block_height = 4, x = 0, y = 7, refresh_interval = 'minute')
-client.add_graphics_submessage('side', 'text', text = "%H:%M", timestring = True, size = 22, x = 30, refresh_interval = 'minute')
+client.add_graphics_submessage('side', 'text', text = "%H:%M", timestring = True, font = "Arial Bold", size = 22, x = 30, refresh_interval = 'minute')
 
 client.add_graphics_submessage('front', 'text', text = "%H:%M", timestring = True, font = "Arial Black", size = 22, halign = 'right', refresh_interval = 'minute')
 
